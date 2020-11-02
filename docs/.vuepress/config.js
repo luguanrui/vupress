f0427b2b00a80c81fffe2e6bfc4b8aab09ff6735
lujing@zhixuezhen.com <lujing@zhixuezhen.com>
2020-11-02 15:38:20 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,125 +1,101 @@
-const dayjs = require("dayjs");
+const dayjs = require('dayjs')
 
 module.exports = {
-  title: "Blog",
-  base: "/",
-  dest: "dist",
-  head: [["link", { rel: "icon", href: "/logo.png" }]],
+  title: 'Blog',
+  base: '/',
+  dest: 'dist',
+  head: [['link', { rel: 'icon', href: '/logo.png' }]],
   themeConfig: {
     displayAllHeaders: true,
-    lastUpdated: "上次更新",
+    lastUpdated: '上次更新',
     sidebarDepth: 3, // 1
     nav: [
       {
-        text: "首页",
-        link: "/",
+        text: '首页',
+        link: '/',
       },
       {
-        text: "基础",
-        link: "/base/",
+        text: '基础',
+        link: '/base/',
       },
       {
-        text: "框架",
+        text: '框架',
         items: [
           {
-            text: "Vue",
+            text: 'Vue',
             items: [
-              { text: "v2", link: "/frame/vue/v2/" },
-              { text: "Vuex", link: "/frame/vue/vuex/" },
-              { text: "VueRouter", link: "/frame/vue/vue-router/" },
+              { text: 'v2', link: '/frame/vue/v2/' },
+              { text: 'Vuex', link: '/frame/vue/vuex/' },
+              { text: 'VueRouter', link: '/frame/vue/vue-router/' },
             ],
           },
           {
-            text: "React",
+            text: 'React',
             items: [
-              { text: "React", link: "/frame/react/react/" },
-              { text: "ReactRouter", link: "/frame/react/react-router/" },
-              { text: "Redux", link: "/frame/react/redux/" },
+              { text: 'React', link: '/frame/react/react/' },
+              { text: 'ReactRouter', link: '/frame/react/react-router/' },
+              { text: 'Redux', link: '/frame/react/redux/' },
             ],
           },
         ],
       },
       {
-        text: "打包工具",
+        text: '打包工具',
         items: [
           {
-            text: "Webpack",
-            link: "/bundle-tool/webpack/",
+            text: 'Webpack',
+            link: '/bundle-tool/webpack/',
             items: [
               {
-                text: "v2",
-                link: "/bundle-tool/webpack/v2",
+                text: 'v2',
+                link: '/bundle-tool/webpack/v2',
               },
               {
-                text: "v4",
-                link: "/bundle-tool/webpack/v4",
+                text: 'v4',
+                link: '/bundle-tool/webpack/v4',
               },
             ],
           },
-          { text: "Rollup", link: "/bundle-tool/rollup/" },
-          { text: "Gulp", link: "/bundle-tool/gulp/" },
+          { text: 'Rollup', link: '/bundle-tool/rollup/' },
+          { text: 'Gulp', link: '/bundle-tool/gulp/' },
         ],
       },
       {
-        text: "分享",
-        link: "/share/",
+        text: '分享',
+        link: '/share/',
       },
       {
-        text: "工具函数",
-        link: "https://github.com/luguanrui/utils-library",
+        text: '工具函数',
+        link: 'https://github.com/luguanrui/utils-library',
       },
       {
-        text: "GitHub",
-        link: "https://github.com/luguanrui?tab=repositories",
+        text: 'GitHub',
+        link: 'https://github.com/luguanrui?tab=repositories',
       },
     ],
     sidebar: {
-      "/base/": [
-        ["", "目录"],
-        "html",
-        "layout",
-        "css",
-        "scope",
-        "dom",
-        "prototype-chain",
-        "communications",
-        "http",
-        "safe",
-        "algorithm",
-        "event-loop",
-        "page-performance",
-        "error",
-        "change-this",
-        "debounce-throttle",
-        "array",
-        "object",
-        "string",
-        "es6",
-      ],
-      "/share/": [
-        ["", "目录"],
-        "vue",
-        "javascript",
-        "css",
-        "document-flow-download",
-        "vue-lazyload",
-        "auto-deploy",
-        "hexo",
-      ],
-      "/frame/vue/v2/": [["", "目录"], "reactive","render", "extend"],
+      '/base/': [['', '目录'], 'html', 'layout', 'css', 'scope', 'dom', 'prototype-chain', 'communications', 'http', 'safe', 'algorithm', 'event-loop', 'page-performance', 'error', 'change-this', 'debounce-throttle', 'array', 'object', 'string', 'es6'],
+      '/share/': [['', '目录'], 'vue', 'javascript', 'css', 'document-flow-download', 'vue-lazyload', 'auto-deploy', 'hexo'],
+      '/frame/vue/v2/': [['', '目录'], 'reactive', 'render', 'extend'],
     },
   },
   plugins: [
-    "@vuepress/medium-zoom",
-    "@vuepress/back-to-top",
-    "vuepress-plugin-nprogress",
+    '@vuepress/medium-zoom', // 图片缩放
+    '@vuepress/back-to-top', // 页面滚动到顶部
+    'vuepress-plugin-nprogress', // 进度条
     [
-      "@vuepress/last-updated",
+      '@vuepress/last-updated',
       {
         transformer: (timestamp, lang) => {
-          return dayjs(timestamp).format("YYYY-MM-DD HH:mm:ss");
+          return dayjs(timestamp).format('YYYY-MM-DD HH:mm:ss')
         },
       },
     ],
+    // 赋值代码
+    [
+      'one-click-copy', {
+        copyMessage: '复制成功'
+      }
+    ]
   ],
-};
+}
--- docs/README.md
+++ docs/README.md
@@ -5,12 +5,12 @@ heroText: Blog
 tagline: 该博客用于记录个人前端学习以及工作积累的过程，为以后走向人生巅峰，赢取白富美做准备！
 actionText: 走你 →
 actionLink: /base/html
-features:
-- title: 基础
-  details: 基础知识是前端人必须掌握牢的东西，打牢巩固基础知识点是非常重要的
-- title: 框架
-  details: 伴随着前端的不断发展，前端的框架也是层出不穷，比如vue，react等，需要我们学习的东西也是越来越多
-- title: 打包工具
-  details: 工欲善其事必先利其器，学好打包工具是一名合格的前端的必备技能
+# features:
+# - title: 基础
+#   details: 基础知识是前端人必须掌握牢的东西，打牢巩固基础知识点是非常重要的
+# - title: 框架
+#   details: 伴随着前端的不断发展，前端的框架也是层出不穷，比如vue，react等，需要我们学习的东西也是越来越多
+# - title: 打包工具
+#   details: 工欲善其事必先利其器，学好打包工具是一名合格的前端的必备技能
 footer: MIT Licensed | Copyright © 2020-present GuanRui Lu
 ---
